4c75b49bfc098a265caf70435cb18392ed1157f6
kanivetuchiha <erickazambuja.ms@gmail.com>
2025-11-25 17:22:17 -0400

    get nome

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ app.get('/req', (req, res) => {
     res.send('Yo!')
 })
 
+app.get('/meunome', (req, res) => {
+    res.send("Meu nome é Erick Azambuja!");
+});
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
